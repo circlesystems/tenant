89f0e7f02df332196b4c55c30dd17c8bc6773d60
DevIsha-a <EishaTabassum0@gmail.com>
2024-02-27 14:39:25 +0500

    fixes in emails

--- obj/Debug/net8.0/circleaccess.AssemblyInfo.cs
+++ obj/Debug/net8.0/circleaccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("circleaccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c78496325d60116c2746784e16c59dac95a222c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4492900b703166b2030bc85314e46b0a41b52b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("circleaccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("circleaccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
